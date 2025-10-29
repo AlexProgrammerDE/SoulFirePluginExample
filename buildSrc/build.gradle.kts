8bdaaec378c6ff3ec9129c20bb221f945817c0ca
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-10-29 21:42:50 +0100

    fix: do replacements

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,4 +17,5 @@ dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.0.0")
     implementation("xyz.wagyourtail.unimined:xyz.wagyourtail.unimined.gradle.plugin:1.4.1")
     implementation("org.ow2.asm:asm:9.9")
+    implementation("net.kyori:indra-git:4.0.0")
 }
--- buildSrc/src/main/kotlin/sf.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/sf.java-conventions.gradle.kts
@@ -2,6 +2,42 @@ plugins {
     `java-library`
     `maven-publish`
     id("sf.license-conventions")
+    id("net.kyori.indra.git")
+}
+
+tasks {
+    // Variable replacements
+    processResources {
+        filesMatching(listOf("fabric.mod.json")) {
+            expand(
+                mapOf(
+                    "version" to project.version,
+                    "description" to project.description,
+                    "commit" to (indraGit.commit().orNull?.name ?: "unknown"),
+                    "branch" to (indraGit.branchName().orNull ?: "unknown"),
+                )
+            )
+        }
+    }
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.compilerArgs.addAll(
+            listOf(
+                "-parameters",
+                "-nowarn",
+                "-Xlint:-unchecked",
+                "-Xlint:-deprecation",
+                "-Xlint:-processing"
+            )
+        )
+        options.isFork = true
+    }
+    test {
+        reports.junitXml.required = true
+        reports.html.required = true
+        useJUnitPlatform()
+        maxParallelForks = Runtime.getRuntime().availableProcessors().div(2).coerceAtLeast(1)
+    }
 }
 
 java {
