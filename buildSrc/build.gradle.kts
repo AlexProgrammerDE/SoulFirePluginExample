649f2ddf5c8b5f1eedd875aef66986b8d7a2d769
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-10-29 16:11:23 +0100

    fix: make remapping work

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,4 +16,5 @@ repositories {
 dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.0.0")
     implementation("xyz.wagyourtail.unimined:xyz.wagyourtail.unimined.gradle.plugin:1.4.1")
+    implementation("org.ow2.asm:asm:9.9")
 }
